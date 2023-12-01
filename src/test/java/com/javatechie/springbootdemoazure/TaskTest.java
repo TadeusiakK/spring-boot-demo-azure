8a5b0ef0c3062d1dc1d099ce8c3e81c4b498a024
Krzysztof Tadeusiak <01169638@pw.edu.pl>
2023-12-01 03:39:06 +0100

    Added good TaskTest

--- src/test/java/com/javatechie/springbootdemoazure/TaskTest.java
+++ src/test/java/com/javatechie/springbootdemoazure/TaskTest.java
@@ -9,7 +9,7 @@ public class TaskTest {
     @Test
     void addTaskNameTest(){
         Task task = new Task("Task1", "Details1", "User1");
-        task.setName("Task5");
+        task.setName("Task1");
 
         assertEquals("Task1", task.getName());
     }
